364d43af6de2b947dd518447cb046cb946d420bf
Ariel_Santangelo <78393362+ariel3259@users.noreply.github.com>
2022-06-24 10:28:28 -0300

    Fixed some bugs

--- src/main/java/com/ariel/ApiSocialMedia/Configures/SecurityConfigure.java
+++ src/main/java/com/ariel/ApiSocialMedia/Configures/SecurityConfigure.java
@@ -2,6 +2,7 @@ package com.ariel.ApiSocialMedia.Configures;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.ariel.ApiSocialMedia.Utils.Jwt.JwtFilter;
 
+@Configuration
 public class SecurityConfigure  {
     
     @Autowired
@@ -26,7 +28,7 @@ public class SecurityConfigure  {
     }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain httpFilterChain(HttpSecurity http) throws Exception {
         http.cors()
             .and()
             .csrf().disable()
