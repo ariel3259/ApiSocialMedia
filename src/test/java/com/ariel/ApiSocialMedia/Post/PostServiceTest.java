177a59f3ff5d7368eddf5ab81dc8c139eada560a
Ariel_Santangelo <78393362+ariel3259@users.noreply.github.com>
2022-07-01 14:28:00 -0300

    deleted unused dependencies

--- src/test/java/com/ariel/ApiSocialMedia/Post/PostServiceTest.java
+++ src/test/java/com/ariel/ApiSocialMedia/Post/PostServiceTest.java
@@ -1,10 +1,6 @@
 package com.ariel.ApiSocialMedia.Post;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
